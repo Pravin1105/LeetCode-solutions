3dbd53d160c3aac91c58b91df7a7c9e5aca768db
Pravin1105 <90402762+Pravin1105@users.noreply.github.com>
2026-07-10 22:17:31 +0530

    Time: 0 ms (100%), Space: 76.6 MB (96.75%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.cpp
+++ 0002-add-two-numbers/0002-add-two-numbers.cpp
@@ -16,11 +16,14 @@ public:
         ListNode* temp2 = l2;
         ListNode* ans = new ListNode();
         ListNode* temp = ans;
+        ListNode* del;
         int carry = 0, sum = 0;
         while(temp1 != NULL && temp2 != NULL){
             sum = temp1->val + temp2->val + carry;
-            ListNode* node = new ListNode(sum%10);
-            temp->next = node;
+            // ListNode* node = new ListNode(sum%10);
+            temp->val = sum%10;
+            del = temp;
+            temp->next = temp1;
             temp = temp->next;
             temp1 = temp1->next;
             temp2 = temp2->next;
@@ -28,28 +31,37 @@ public:
         }
         while(temp1 != NULL){
             sum = temp1->val + carry;
-            ListNode* node = new ListNode(sum%10);
-            temp->next = node;
+            // ListNode* node = new ListNode(sum%10);
+            temp->val = sum%10;
+            del = temp;
+            temp->next = temp1;
             temp = temp->next;
             carry = sum/10;
             temp1 = temp1->next;
         }
         while(temp2 != NULL){
             sum = temp2->val + carry;
-            ListNode* node = new ListNode(sum%10);
-            temp->next = node;
+            // ListNode* node = new ListNode(sum%10);
+            del = temp;
+            temp->val = sum%10;
+            temp->next = temp2;
             temp = temp->next;
             carry = sum/10;
             temp2 = temp2->next;
         }
         if(carry != 0){
-            ListNode* node = new ListNode(carry);
-            temp->next = node;
+            // ListNode* node = new ListNode(carry);
+            temp->val = carry;
+            temp->next = NULL;
 
         }
-        temp = ans;
-        ans = ans->next;
-        delete temp;
+        else{
+            del->next = NULL;
+            delete temp;
+        }
+        // temp = ans;
+        // ans = ans->next;
+        // delete temp;
         return ans;
     }
 };
